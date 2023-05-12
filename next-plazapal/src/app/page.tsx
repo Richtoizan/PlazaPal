eefd3c95926b6c3b602145fff012d0559741106a
Zaid Khirfan <Khirfan@hotmail.com>
2023-05-12 18:08:13 +0300

    Initializing the app and configuring the structure

---
+++ next-plazapal/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>PlazaPal</title>
+        <meta name="description" content="PlazaPal Application" />
+      </Head>
+
+      <main>
+        <h1>Welcome to PlazaPal</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;

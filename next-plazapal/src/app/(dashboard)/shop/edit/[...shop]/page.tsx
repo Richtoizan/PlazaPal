f1977b590eaa98150a0bf4705e6ff284188a9c63
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2023-06-10 00:25:38 +0300

    Add editing page and API for Shop

---
+++ next-plazapal/src/app/(dashboard)/shop/edit/[...shop]/page.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/Button";
+import React, { useEffect, useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { ChangeEvent } from "react";
+import { MenuItem, Select, SelectChangeEvent } from "@mui/material";
+
+export default function Page() {
+  const [saveShop, setSaveShop] = useState({
+    name: "",
+    sector: "",
+    ownedBy: "",
+  });
+
+  const router = useRouter();
+
+  const pathNames = usePathname().split('/');
+  const shopId = pathNames[pathNames.length - 1];
+
+  const [owners, setOwners] = useState([{
+    id: "",
+    name: "",
+    surname: "",
+  }]);
+
+  useEffect(() => {
+    const fetchOwners = async () => {
+      const endpoint = "/api/shopOwner";
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await fetch(endpoint, options);
+      const result = await response.json();
+
+      setOwners(result);
+    }
+
+    const fetchShop = async () => {
+      const endpoint = "/api/shop?id=" + shopId;
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const response = await fetch(endpoint, options);
+      const result = await response.json();
+
+      setSaveShop(result);
+    }
+
+    fetchShop();
+    fetchOwners();
+  }, []);
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+
+    const dataToSend = { ...saveShop, ownedBy: Number(saveShop.ownedBy) };
+
+    const endpoint = "/api/shop?id=" + shopId;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToSend),
+    };
+
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+
+    if (result) {
+    }
+
+    router.push("/shop/" + shopId);
+  };
+
+  const handleSaveChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSaveShop({ ...saveShop, [name]: value });
+  };
+
+  const handleSaveChangeSelect = (event: SelectChangeEvent) => {
+    const { name, value } = event.target;
+    setSaveShop({ ...saveShop, [name]: value });
+  };
+
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <Card>
+            <CardHeader>
+              <CardTitle>Edit Shop</CardTitle>
+              <CardDescription>
+                Edit the details of the shop
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form onSubmit={handleSubmit}>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      name="name"
+                      id="name"
+                      value={saveShop.name}
+                      onChange={handleSaveChange}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="sector">Sector</Label>
+                    <Input
+                      name="sector"
+                      id="sector"
+                      value={saveShop.sector}
+                      onChange={handleSaveChange}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="ownedBy">Owner</Label>
+                    <Select
+                      name="ownedBy"
+                      labelId="ownedBy-select-label"
+                      id="ownedBy-select"
+                      value={saveShop.ownedBy}
+                      label="Owner"
+                      onChange={handleSaveChangeSelect}
+                    >
+                      {owners.map(owner => (
+                        <MenuItem key={owner.id} value={owner.id}>
+                          {owner.name} {owner.surname}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </div>
+                  <Button type="submit">Submit</Button>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+          <div className="relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute"></div>
+        </div>
+        <div className="relative h-screen flex items-center justify-center overflow-x-hidden"></div>
+      </div>
+    </div>
+  );
+}
--- next-plazapal/src/pages/api/shop.ts
+++ next-plazapal/src/pages/api/shop.ts
@@ -8,21 +8,85 @@ function replacer(key: any, value: any) {
   return value;
 }
 
-// POST /api/shop
+// /api/shop
 // Required fields in body: sector, name, ownedBy
 export default async function handle(req: any, res: any) {
-  const { sector, name, ownedBy } = req.body;
+  if (req.method === 'GET') {
+    const { id } = req.query;
 
-  const result = await db.shop.create({
-    data: {
-      Sector: sector,
-      Name: name,
-      OwnedBy: ownedBy,
-    },
-  });
+    if (id) {
+      // Handle GET request for a specific shop by ID
+      const shop = await db.shop.findUnique({
+        where: {
+          ID: Number(id),
+        },
+        include: {
+          ShopOwner: {
+            select: {
+              Name: true,
+              Surname: true,
+            }
+          }
+        }
+      });
 
-  // Use the replacer function when stringifying the result
-  const resultString = JSON.stringify(result, replacer);
+      if (shop) {
+        res.status(200).json({
+          name: shop.Name,
+          sector: shop.Sector,
+          ownedBy: Number(shop.OwnedBy),
+          ownerName: shop.ShopOwner.Name + " " + shop.ShopOwner.Surname
+        });
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } else {
+      // Handle GET request for all shops
+      const shops = await db.shop.findMany();
 
-  res.json(JSON.parse(resultString));
+      res.status(200).json(shops.map(shop => ({
+          name: shop.Name,
+          sector: shop.Sector,
+          ownedBy: Number(shop.OwnedBy),
+        })));
+    }
+  }
+  else if (req.method === 'POST') {
+    const { sector, name, ownedBy } = req.body;
+    const { id } = req.query;
+
+    if (id) {
+      const result = await db.shop.update({
+        where: {
+          ID: Number(id),
+        },
+        data: {
+          Sector: sector,
+          Name: name,
+          OwnedBy: ownedBy,
+        }
+      })
+
+      const resultString = JSON.stringify(result, replacer);
+      res.json(JSON.parse(resultString));
+    }
+    else {
+
+      const result = await db.shop.create({
+        data: {
+          Sector: sector,
+          Name: name,
+          OwnedBy: ownedBy,
+        },
+      });
+    
+      // Use the replacer function when stringifying the result
+      const resultString = JSON.stringify(result, replacer);
+    
+      res.json(JSON.parse(resultString));
+    }
+  }
+  else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
 }
